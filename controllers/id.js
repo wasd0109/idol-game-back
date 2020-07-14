a71cce0e9a3bc1aff163cbf181c35882af4f43c1
Ken Cheung <wasd0109.dev@gmail.com>
2020-07-15 00:29:47 +0800

    Change for heroku debug

--- controllers/id.js
+++ controllers/id.js
@@ -1,9 +1,13 @@
 const handleGetProfile = (pg) => (req, res) => {
   const { id } = req.params;
+  console.log(id);
   pg.select('*')
     .from('players')
     .where('userid', '=', Number(id))
-    .then((data) => res.json(data))
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
     .catch(console.log);
 };
 
